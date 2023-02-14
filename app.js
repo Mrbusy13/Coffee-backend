7dc93f8442b59eac4b93eb7c6f870de05a624462
Mrbusy <nickhughes13@googlemail.com>
2023-02-14 17:12:25 +0000

    app.js added importing express and setting up the listener

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+const app = express()
+const PORT = 3000
+
+
+app.use(cors())
+app.use(express.static("public"));
+app.use(express.json());
+
+app.listen(PORT, function () {
+    console.log(`Server is listening on http://localhost:${PORT}`);
+  });
+
+export default app;
