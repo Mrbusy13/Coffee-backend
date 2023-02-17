34c782992404883fedf4e2f3f91e9f77936b5358
Mrbusy <nickhughes13@googlemail.com>
2023-02-17 15:29:12 +0000

    Typescript added, index.ts updated with no errors

--- server.ts
+++ server.ts
@@ -2,15 +2,17 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose"
 import router from "./routes/coffeeshops.js";
+import { NextFunction } from "express";
 
 //express app
 const app = express();
-const PORT = process.env.PORT;
+const PORT: any = process.env.PORT;
+const ATLAS_URI: any = process.env.ATLAS_URI
 
 //middleware
 app.use(cors());
 app.use(express.json());
-app.use((req, res, next)=>{
+app.use((req: express.Request, res: express.Response, next: NextFunction)=>{
   console.log(req.path, req.method)
   next();
 })
@@ -19,7 +21,7 @@ app.use((req, res, next)=>{
 app.use("/api/coffeeshops", router)
 
 //connect to db;
-mongoose.connect(process.env.ATLAS_URI)
+mongoose.connect(ATLAS_URI)
 .then(()=> {
 //listen for requests
   app.listen(PORT, ()=> {
