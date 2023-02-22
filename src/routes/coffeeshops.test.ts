ff243be6a528b7d801bec8d9d7d3c1ce05746557
Mrbusy <nickhughes13@googlemail.com>
2023-02-22 16:53:12 +0000

    supertest installed, coffeeshop.test.ts file created and initial get all test written

---
+++ src/routes/coffeeshops.test.ts
@@ -0,0 +1,19 @@
+import request from "supertest"
+import app from "../server.js"
+import {expect, test} from "@jest/globals"
+
+describe('GET /', function(){
+    it('responds with list of all coffeeshops', async function() {
+        const response = await request(app).get('/api/coffeeshops')
+        console.log(response)
+        expect(response.status).toEqual(200)
+        expect (response.body).toStrictEqual(expect.any(Array))
+        for (let i=0; i<response.body.length[i]; i++){
+            const coffeeshopObject = response.body[i]
+            console.log(coffeeshopObject)
+                expect(coffeeshopObject).toContain({
+                    name: expect.any(String), town: expect.any(String), //completion_date: expect.any(String),
+                    })
+        }
+    })
+})
